6a420ba28c847701e806f998a6353807dfe5e9a3
AiarCost <65986842+AiarCost@users.noreply.github.com>
2021-12-06 15:43:58 -0700

    initial build and project import

---
+++ Rigging_Characters_203/Assets/AstraInput_script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstraInput_script : MonoBehaviour
+{
+    private Animator astraAnimator = null;
+    private float verticalPress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        astraAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalPress = Input.GetAxis("Vertical");
+        astraAnimator.SetFloat("Blend", verticalPress);
+    }
+}
